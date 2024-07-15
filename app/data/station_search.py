edce5a3a3296c50235d43c21778e73b8d7f4a70a
Fletcher B Sims <fletcher.sims@gmail.com>
2024-07-15 07:07:58 -0400

    add in code to grab station meta data

--- app/data/station_search.py
+++ app/data/station_search.py
@@ -48,29 +48,38 @@ def parse_json(data):
     return result
 
 
+def parse_station_data(data):
+    report_name = [
+        variable['actualName']
+        for station in data['stations']
+        for variable in station['variable']
+    ]
+    report_name.insert(0, 'time_utc')
+    measurements = [
+        msr['value']
+        for station in data['stations']
+        for variable in station['variable']
+        for msr in variable['measurements']
+    ]
+    time_utc = list({
+        msr['time']
+        for station in data['stations']
+        for variable in station['variable']
+        for msr in variable['measurements']
+    })
+    measurements.insert(0, time_utc[0])
+    return dict(zip(report_name, measurements))
+
+
 # station = StationSearch(os.getenv('CBIBS_API_KEY'), 'AN')
 # tmp = parse_json(station.grab_station_data())
+
 tmp = parse_json(json.loads(sample))
+print(tmp)
+station_short_name = [station['stationShortName'] for station in tmp['stations']]
+station_long_name = [station['stationLongName'] for station in tmp['stations']]
+op_state = [station['opState'] for station in tmp['stations']]
 
-# report_name = tmp['stations'][0]['variable'][1]['reportName']
-
-report_name = [
-    variable['actualName']
-    for station in tmp['stations']
-    for variable in station['variable']
-]
-report_name.insert(0, 'time_utc')
-measurements = [
-    msr['value']
-    for station in tmp['stations']
-    for variable in station['variable']
-    for msr in variable['measurements']
-]
-time_utc = list({
-    msr['time']
-    for station in tmp['stations']
-    for variable in station['variable']
-    for msr in variable['measurements']
-})
-measurements.insert(0, time_utc[0])
-print(dict(zip(report_name, measurements)))
+print(station_short_name)
+print(station_long_name)
+print(op_state)
